66a1d867c7f7ddf6e15ce0fdc6050d41ac2e679e
TexasOct <aecom233@gmail.com>
2022-04-01 23:05:30 +0800

    version 1.0

---
+++ ncm_to_flac.py
@@ -0,0 +1,35 @@
+# Python scrip to trans ncm to flac
+import os
+import pyinotify
+import time
+
+# Prepare the file path
+NCM_converter_path = "/home/texas/NCMconverter"
+files_path = "/home/texas/Music/CloudMusic"
+target_type = "*.ncm"
+
+# System command
+cmd = f"./NCMconverter -o {files_path} {files_path}/*.ncm"
+rm = f"rm {files_path}/{target_type}"
+
+# Listen the files_path
+event = pyinotify.IN_MOVED_TO
+watcher = pyinotify.WatchManager()
+
+
+class EventHandler(pyinotify.ProcessEvent):
+    # Trans the .ncm to .flac
+    def process_IN_MOVED_TO(self, event):
+        if any(name.endswith(target_type) for name in os.listdir(files_path)):
+            os.chdir(NCM_converter_path)
+            os.system(cmd)
+            time.sleep(60)
+            os.system(rm)
+            print("Transfer complete!")
+
+
+if __name__ == '__main__':
+    handler = EventHandler()
+    watcher.add_watch(f'{files_path}', event, rec=True)
+    notifier = pyinotify.Notifier(watcher, handler)
+    notifier.loop()
